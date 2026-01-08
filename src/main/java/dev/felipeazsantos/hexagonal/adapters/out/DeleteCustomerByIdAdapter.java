122d08e360a76e502812a8ad446800c3b3af667d
felipeazsantos <felipeasantos10@hotmail.com>
2026-01-08 12:55:32 -0300

    delete customer adapter

--- src/main/java/dev/felipeazsantos/hexagonal/adapters/out/DeleteCustomerByIdAdapter.java
+++ src/main/java/dev/felipeazsantos/hexagonal/adapters/out/DeleteCustomerByIdAdapter.java
@@ -3,7 +3,9 @@ package dev.felipeazsantos.hexagonal.adapters.out;
 import dev.felipeazsantos.hexagonal.adapters.out.repository.CustomerRepository;
 import dev.felipeazsantos.hexagonal.application.ports.out.DeleteCustomerByIdOutputPort;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class DeleteCustomerByIdAdapter implements DeleteCustomerByIdOutputPort {
 
     @Autowired
