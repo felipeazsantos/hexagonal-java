aefddafa4910288597b5ef3eb872450253ecd216
felipeazsantos <felipeasantos10@hotmail.com>
2026-01-09 11:29:26 -0300

    ArchUnit testing layer app

---
+++ src/test/java/dev/felipeazsantos/hexagonal/architecture/LayeredArchitectureTest.java
@@ -0,0 +1,29 @@
+package dev.felipeazsantos.hexagonal.architecture;
+
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.lang.ArchRule;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static com.tngtech.archunit.library.Architectures.layeredArchitecture;
+
+@AnalyzeClasses(packages = "dev.felipeazsantos.hexagonal")
+@SpringBootTest
+public class LayeredArchitectureTest {
+
+    @ArchTest
+    public static final ArchRule layered_architecture_test = layeredArchitecture()
+            .consideringAllDependencies()
+            .layer("AdaptersIn").definedBy("..adapters.in..")
+            .layer("AdaptersOut").definedBy("..adapters.out..")
+            .layer("UseCase").definedBy("..application.core.usecase..")
+            .layer("PortsIn").definedBy("..application.ports.in..")
+            .layer("PortsOut").definedBy("..application.ports.out..")
+            .layer("Config").definedBy("..config..")
+            .whereLayer("AdaptersIn").mayOnlyBeAccessedByLayers("Config")
+            .whereLayer("AdaptersOut").mayOnlyBeAccessedByLayers("Config")
+            .whereLayer("UseCase").mayOnlyBeAccessedByLayers("Config")
+            .whereLayer("PortsIn").mayOnlyBeAccessedByLayers("UseCase", "AdaptersIn", "Config")
+            .whereLayer("PortsOut").mayOnlyBeAccessedByLayers("UseCase", "AdaptersOut", "Config")
+            .whereLayer("Config").mayNotBeAccessedByAnyLayer();
+}
---
+++ src/test/java/dev/felipeazsantos/hexagonal/architecture/NamingConventionTest.java
@@ -0,0 +1,4 @@
+package dev.felipeazsantos.hexagonal.architecture;
+
+public class NamingConventionTest {
+}
